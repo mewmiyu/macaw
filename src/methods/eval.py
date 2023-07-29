f203854efb5c739b46456d6787cac1b79e6cdee5
DanailIordanov <danail.iordanov@yahoo.com>
2023-07-29 16:43:55 +0200

    Add endpoint for frame evaluation

--- src/methods/eval.py
+++ src/methods/eval.py
@@ -1,6 +1,7 @@
 import numpy as np
 import time
 import torch
+from numpy.typing import NDArray
 from torch.utils.data import DataLoader
 from typing import Any, Tuple
 
@@ -41,10 +42,40 @@ class TorchImageProvider(ImageProvider):
         )
 
     def __call__(self, silent=True) -> Tuple[Any, ...]:
-        self.data, targets = next(iter(self.data_loader))
-        self.images = list(image["image"].to(self.device) for image in self.data)
-        self.image_names = list(image["filename"] for image in self.data)
+        data, targets = next(iter(self.data_loader))
+        images = list(image["image"].to(self.device) for image in data)
+        image_names = list(image["filename"] for image in data)
         targets = [{k: v for k, v in t.items()} for t in targets]
+
+        start_time = time.time()
+        predictions = self.model(images)
+        inference_time = time.time() - start_time
+
+        if not silent:
+            print("[INFO] Inference time", inference_time)
+
+        return (
+            np.array(images[0].detach().to("cpu").permute((1, 2, 0))),
+            targets[0]["boxes"],
+            predictions[0],
+            image_names[0],
+        )
+
+
+class PredictionsProvider(ImageProvider):
+    def __init__(self, model_checkpoint=None, device="cpu") -> None:
+        super().__init__()
+
+        self.device = device
+        if model_checkpoint is not None:
+            self.model = torch.load(model_checkpoint, map_location=self.device)
+            self.model.eval()
+        else:
+            raise ValueError(f"No model checkpoint was provided!")
+
+    def __call__(self, image: NDArray, silent=True) -> Any:
+        image_tensor = torch.from_numpy(image).to(self.device)
+        self.images = [image_tensor]
 
         start_time = time.time()
         predictions = self.model(self.images)
@@ -55,7 +86,7 @@ class TorchImageProvider(ImageProvider):
 
         return (
             np.array(self.images[0].detach().to("cpu").permute((1, 2, 0))),
-            targets[0]["boxes"],
+            None,
             predictions[0],
-            self.image_names[0],
+            None,
         )
