c5f9d64b0ec295885a6f2356d1fda8182e25be2d
Johannes Beck <johannes_beck@arcor.de>
2023-07-30 11:48:00 +0200

    Integrated Cropping and put tracking BEFORE the detector/matching loop

--- src/macaw.py
+++ src/macaw.py
@@ -48,6 +48,9 @@ def macaw():
     # loop over frames from the video file stream
     while True:  # fvs.more():
         count +=1
+        bbox = None
+        crop_offset = np.array([[[0, 0]]])
+
         # grab the frame from the threaded video file stream, resize
         # it, and convert it to grayscale (while still retaining 3
         # channels)
@@ -63,38 +66,39 @@ def macaw():
         frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         frame_gray = cv.UMat(cv.GaussianBlur(frame_gray, (5, 5), 0))
 
+        valid = False
+        # tracking:
+        if count % matching_rate != 0 and pts_f is not None and len(pts_f) >0:
+            pts_f, pts_m, valid = features.track(last_frame_gray, frame_gray, pts_f, pts_m)
+            bbox = features.calc_bounding_box(matches, masks[mask_id], pts_f, pts_m)
+
+        last_frame_gray = frame_gray
 
         boxes, labels, scores = detector.run_detector(frame)  # , showHits=True
         hit, label, box = detector.filter_hits(boxes, labels, scores)
         # TODO: Filter + Crop boxes
         # TODO: Check order of the new box! (xy min max)
         box_pixel = np.array(box * np.asarray(frame.shape[:2]), dtype=int).flatten()
-        # crop_offset = np.array(box_pixel[1], box_pixel[0]).reshape((1, 1, 2))
-        if True:  # hit and (box_pixel[2] - box_pixel[0]) * (box_pixel[3] - box_pixel[1]) > 0:  # hit and non-zero patch
+        if not valid and hit and (box_pixel[2] - box_pixel[0]) * (box_pixel[3] - box_pixel[1]) > 0:  # hit and non-zero patch
+
+            # Crop the img
+            crop_offset = np.array([[[box_pixel[1], box_pixel[0]]]])
             cropped = utils.crop_img(frame, *box_pixel.flatten())  # Test cropping and apply
-            valid = False
-            # tracking:
-            if count % matching_rate != 0 and pts_f is not None and len(pts_f) >0:
-                pts_f, pts_m, valid = features.track(last_frame_gray, frame_gray, pts_f, pts_m)
+            frame_gray = cv.UMat(cv.GaussianBlur(cv.cvtColor(cropped, cv.COLOR_BGR2GRAY), (5, 5), 0))
 
+            # match the features of the cropped img
             kp, des = compute_feature(frame_gray)
+            matches, mask_id = features.match(des, masks)
+            pts_f, pts_m = features.get_points_from_matched_keypoints(matches, kp, masks[mask_id].kp)
 
-            # TODO: UMat
-            # uframe = cv.UMat(cropped)  # cropped #
-
-            if not valid:
-                # rendering.display_image(cropped)
-                matches, mask_id = features.match(des, masks)
-                pts_f, pts_m = features.get_points_from_matched_keypoints(matches, kp, masks[mask_id].kp)
-
-            last_frame_gray = frame_gray
-            # homography
+            # gt the bounding box (None, with/without homography -> depends on number of hits)
             bbox = features.calc_bounding_box(matches, masks[mask_id], pts_f, pts_m)
-            if bbox is not None:
 
-                frame = rendering.render_contours(frame_umat, np.int32(bbox))
-                frame = rendering.render_fill_contours(frame, np.int32(bbox))
-                frame = rendering.render_metadata(frame, 'mask_Hauptgebaeude_no_tree')  # label
+        if bbox is not None:
+            bbox += crop_offset  # bbox is calculated of the croped image -> global coordinates by adding the offset
+            frame = rendering.render_contours(frame_umat, np.int32(bbox))
+            frame = rendering.render_fill_contours(frame, np.int32(bbox))
+            frame = rendering.render_metadata(frame, 'mask_Hauptgebaeude_no_tree')  # label
 
         # show the frame and update the FPS counter
         rendering.render_text(frame, "FPS: {:.2f}".format(1.0 / (time.time() - time_start)), (10, frame_size[0] - 10))
